f5822aa8f14e4cdb288b6555443bcd5eecbad5ad
TrustiestMass28 <fabiano.sasselli@gmail.com>
2025-05-02 20:50:35 +0200

    save

--- Source/AmrDG_Level.cpp
+++ Source/AmrDG_Level.cpp
@@ -44,8 +44,9 @@ void AmrDG::AMR_remake_level(int lev, amrex::Real time, const amrex::BoxArray& b
   for(int q=0 ; q<Q; ++q){
     _mf[q].define(ba, dm, basefunc->Np_s, _mesh->nghost);
     _mf[q].setVal(0.0);    
-    //FillPatch(lev, time, _mf[q], 0, basefunc->Np_s,q);  //TODO
   } 
+  
+  AMR_FillPatch(lev, time, _mf, 0, basefunc->Np_s);
 
   //clear existing level MFabs defined on old ba,dm
   _mesh->ClearLevel(lev);
@@ -73,7 +74,7 @@ void AmrDG::AMR_make_new_fine_level(int lev, amrex::Real time,
   //create new level MFabs defined on new ba,dm
   set_init_data_system(lev,ba,dm); 
 
-  AMR_FillCoarsePatch(lev, time, U_w[lev], 0, basefunc->Np_s);
+  AMR_FillFromCoarsePatch(lev, time, U_w[lev], 0, basefunc->Np_s);
   //FillPatchGhostFC(lev,time,q);//TODO
   //
 }
@@ -81,7 +82,7 @@ void AmrDG::AMR_make_new_fine_level(int lev, amrex::Real time,
 // fill an entire multifab by interpolating from the coarser level
 // this comes into play when a new level of refinement appears
 //also fills ghost cells
-void AmrDG::AMR_FillCoarsePatch (int lev, Real time, amrex::Vector<amrex::MultiFab>& fmf, 
+void AmrDG::AMR_FillFromCoarsePatch (int lev, Real time, amrex::Vector<amrex::MultiFab>& fmf, 
                                 int icomp,int ncomp)
 {   
   auto _mesh = mesh.lock();
@@ -96,49 +97,21 @@ void AmrDG::AMR_FillCoarsePatch (int lev, Real time, amrex::Vector<amrex::MultiF
   amrex::PhysBCFunct<amrex::CpuBndryFuncFab> coarse_physbcf(_mesh->get_Geom(lev-1),dummy_bc,bcf);
   amrex::PhysBCFunct<amrex::CpuBndryFuncFab> fine_physbcf(_mesh->get_Geom(lev),dummy_bc,bcf);
 
-  //amrex::Interpolater* mapper = &custom_interp;//TODO
+  amrex::Interpolater* mapper= &amrex::cell_cons_interp;//= &custom_interp;//TODO
+
+  amrex::Vector<MultiFab*> cmf;
+  amrex::Vector<Real> ctime;
 
   for(int q=0 ; q<Q; ++q){                            
-    amrex::Vector<MultiFab*> cmf;
-    amrex::Vector<Real> ctime;
-
     //Store tmp data of the coarse MFab
-    //GetData(lev-1,q, time, cmf, ctime);
-    cmf.clear();
-    ctime.clear();
+
     cmf.push_back(&(U_w[lev-1][q]));
     ctime.push_back(time);
 
-    //TODO
-    //amrex::InterpFromCoarseLevel(fmf[q], time, *cmf[0], 0, icomp, ncomp, geom[lev-1], 
-    //    geom[lev], coarse_physbcf, 0, fine_physbcf, 0, 
-    //    refRatio(lev-1),mapper, bc_w[q], 0);      
+    amrex::InterpFromCoarseLevel(fmf[q], time, *cmf[0], 0, icomp, ncomp,_mesh->get_Geom(lev-1), 
+                                _mesh->get_Geom(lev), coarse_physbcf, 0, fine_physbcf, 0, 
+                                _mesh->get_refRatio(lev-1),mapper, dummy_bc, 0);       
   }                         
-}
-
-/*
-void AmrDG::GetData (int lev, int q, Real time, Vector<MultiFab*>& data, 
-                      Vector<Real>& datatime)
-{
-  data.clear();
-  datatime.clear();
-  data.push_back(&(U_w[lev][q]));
-  datatime.push_back(time);
-}
-*/
-
-/*
-//averages cell centered data from finer cells to the respective covered coarse cell
-void AmrDG::AverageFineToCoarse()
-{  
-//Print(*ofs) << "AverageFineToCoarse()"<< "\n";
-
-for (int l = finest_level; l > 0; --l){  
-for(int q=0; q<Q; ++q){   
-custom_interp.average_down(U_w[l][q], U_w[l-1][q],0,U_w[l-1][q].nComp(), 
-              refRatio(l-1), l,l-1);
-}
-} 
 }
 
 //Fillpatch operations fill all cells, valid and ghost, from actual valid data at 
@@ -146,53 +119,66 @@ custom_interp.average_down(U_w[l][q], U_w[l-1][q],0,U_w[l-1][q].nComp(),
 //neighboring grids at the same level, and domain boundary conditions 
 //(for examples that have non-periodic boundary conditions).
 //NB: this function is used for regrid and not for timestepping
-void AmrDG::FillPatch(int lev, Real time, amrex::MultiFab& mf,int icomp, int ncomp, int q)
+void AmrDG::AMR_FillPatch(int lev, Real time, amrex::Vector<amrex::MultiFab>& mf,int icomp, int ncomp)
 {  
-  //Print(*ofs) << "FillPatch   "<< lev<< " |component  "<<q<<"\n";
+
+  auto _mesh = mesh.lock();
+
+  amrex::CpuBndryFuncFab bcf(nullptr); 
+  auto dummy_bc = get_null_BC(ncomp);
 
   if (lev == 0)
-  { 
-    amrex::Vector<MultiFab*> smf;
-    amrex::Vector<Real> stime;
-    GetData(lev, q,time, smf, stime);
-    
-    amrex::CpuBndryFuncFab bcf(nullptr); 
-    amrex::PhysBCFunct<amrex::CpuBndryFuncFab> physbcf(geom[lev],bc_w[q],bcf);
-    
-    amrex::FillPatchSingleLevel(mf, time, smf, stime, 0, icomp, ncomp,geom[lev], physbcf, 0);  
-    //FillPatchSingleLevel()  :   fills a MultiFab and its ghost region at a single 
-    //                            level of refinement. The routine is flexible enough 
-    //                            to interpolate in time between two MultiFabs 
-    //                            associated with different times
-    //                            
-    //                            calls also MultiFab::FillBoundary,
-    //                            MultiFab::FillDomainBoundary()
-    //                            
+  {
+    amrex::PhysBCFunct<amrex::CpuBndryFuncFab> physbcf(_mesh->get_Geom(lev),dummy_bc,bcf);
+
+    for(int q=0 ; q<Q; ++q){  
+      amrex::Vector<MultiFab*> smf;
+      amrex::Vector<Real> stime;
+
+      smf.push_back(&(U_w[lev][q]));
+      stime.push_back(time);  
+
+      amrex::FillPatchSingleLevel(mf[q], time, smf, stime, 0, icomp, ncomp,_mesh->get_Geom(lev), physbcf, 0);  
+      //FillPatchSingleLevel()  :   fills a MultiFab and its ghost region at a single 
+      //                            level of refinement. The routine is flexible enough 
+      //                            to interpolate in time between two MultiFabs 
+      //                            associated with different times
+      //                            
+      //                            calls also MultiFab::FillBoundary,
+      //                            MultiFab::FillDomainBoundary()     
+    }                
   }
   else
   { 
-    amrex::Vector<MultiFab*> cmf, fmf;
-    amrex::Vector<Real> ctime, ftime;
-    GetData(lev-1, q,time, cmf, ctime);
-    GetData(lev  , q,time, fmf, ftime);
+    amrex::Interpolater* mapper = &amrex::cell_cons_interp;//= &custom_interp; TODO
 
-    amrex::Interpolater* mapper = &custom_interp;
-    amrex::CpuBndryFuncFab bcf(nullptr);
-    amrex::PhysBCFunct<amrex::CpuBndryFuncFab> coarse_physbcf(geom[lev-1],bc_w[q],bcf);
-    amrex::PhysBCFunct<amrex::CpuBndryFuncFab> fine_physbcf(geom[lev],bc_w[q],bcf);
-    
-    amrex::FillPatchTwoLevels(mf, time, cmf, ctime, fmf, ftime,0, icomp, ncomp, 
-                              geom[lev-1], geom[lev],coarse_physbcf, 0, fine_physbcf, 
-                              0, refRatio(lev-1),mapper, bc_w[q], 0);
-    //FillPatchTwoLevels()    :   fills a MultiFab and its ghost region at a single 
-    //                            level of refinement, assuming there is an underlying 
-    //                           coarse level. This routine is flexible enough to 
-    //                            interpolate the coarser level in time first using 
-    //                            FillPatchSingleLevel()
+    amrex::PhysBCFunct<amrex::CpuBndryFuncFab> coarse_physbcf(_mesh->get_Geom(lev-1),dummy_bc,bcf);
+    amrex::PhysBCFunct<amrex::CpuBndryFuncFab> fine_physbcf(_mesh->get_Geom(lev),dummy_bc,bcf);
 
+    for(int q=0 ; q<Q; ++q){  
+      amrex::Vector<MultiFab*> cmf, fmf;
+      amrex::Vector<Real> ctime, ftime;
+
+      cmf.push_back(&(U_w[lev-1][q]));
+      ctime.push_back(time);
+
+      fmf.push_back(&(U_w[lev][q]));
+      ftime.push_back(time);
+
+      amrex::FillPatchTwoLevels(mf[q], time, cmf, ctime, fmf, ftime,0, icomp, ncomp, 
+                                _mesh->get_Geom(lev-1), _mesh->get_Geom(lev),
+                                coarse_physbcf, 0, fine_physbcf, 
+                                0, _mesh->get_refRatio(lev-1),mapper, dummy_bc, 0);
+      //FillPatchTwoLevels()    :   fills a MultiFab and its ghost region at a single 
+      //                            level of refinement, assuming there is an underlying 
+      //                           coarse level. This routine is flexible enough to 
+      //                            interpolate the coarser level in time first using 
+      //                            FillPatchSingleLevel()
+    }
   }
 }
 
+/*
 //fills ghost cells of fine level at fine-coarse interface with respective
 //coarse data. Used during timestepping
 void AmrDG::FillPatchGhostFC(int lev,amrex::Real time,int q)
@@ -221,9 +207,22 @@ void AmrDG::FillPatchGhostFC(int lev,amrex::Real time,int q)
   fillpatcher->fillCoarseFineBoundary(U_w[lev][q],IntVect(nghost),time,cmf,ctime,
                                       0,0,Np,coarse_physbcf,0,bc_w[q],0);                        
 }	    
-	    
+*/
 
+/*
+//averages cell centered data from finer cells to the respective covered coarse cell
+void AmrDG::AverageFineToCoarse()
+{  
+//Print(*ofs) << "AverageFineToCoarse()"<< "\n";
 
+for (int l = finest_level; l > 0; --l){  
+for(int q=0; q<Q; ++q){   
+custom_interp.average_down(U_w[l][q], U_w[l-1][q],0,U_w[l-1][q].nComp(), 
+              refRatio(l-1), l,l-1);
+}
+} 
+}
+ 
 //averages face centered data from finer cells to the respective covered coarse cell
 void AmrDG::AverageFineToCoarseFlux(int lev)
 {
